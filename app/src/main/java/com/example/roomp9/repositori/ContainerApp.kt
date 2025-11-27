6905300c6d191bbd201f410ceedcbd053cd0a100
RMadya <kereyoben75@gmail.com>
2025-11-28 01:33:46 +0700

    ContainerApp adalah sebuah interface yang mendeklarasikan properti repositoriSiswa. Properti ini bertipe RepositoriSiswa, yang kemungkinan besar digunakan untuk berinteraksi dengan data siswa. Interface ini berfungsi untuk mendefinisikan kontrak yang harus dipenuhi oleh kelas-kelas yang mengimplementasikannya.

--- app/src/main/java/com/example/roomp9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomp9/repositori/ContainerApp.kt
@@ -1,2 +1,11 @@
 package com.example.roomp9.repositori
 
+import android.app.Application
+import android.content.Context
+import androidx.room.Database
+import com.example.roomp9.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
