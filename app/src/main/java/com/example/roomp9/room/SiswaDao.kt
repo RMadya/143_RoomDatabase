4322de8ea202669c15d0be81fb07eed0b0dbc91f
RMadya <kereyoben75@gmail.com>
2025-11-28 01:39:38 +0700

    Anotasi @Dao menandakan bahwa interface ini adalah Data Access Object (DAO) yang digunakan untuk berinteraksi dengan database Room

--- app/src/main/java/com/example/roomp9/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomp9/room/SiswaDao.kt
@@ -2,14 +2,9 @@ package com.example.roomp9.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
-    fun getAllSiswa(): Flow<List<Siswa>>
-
-    @Insert(onConflict = onConFlictStrategy.IGNORE)
-    suspend fun insert(siswa: Siswa)
-}
