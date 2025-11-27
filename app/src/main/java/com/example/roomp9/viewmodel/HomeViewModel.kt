315a83fd438d7e2fc0e676e15d70578d16c1d1b7
RMadya <kereyoben75@gmail.com>
2025-11-28 02:00:32 +0700

    HomeViewModel adalah ViewModel yang bertanggung jawab untuk mengelola dan menyiapkan data yang akan ditampilkan dalam tampilan HomeScreen (misalnya, daftar siswa).
    repositoriSiswa: Ini adalah dependensi yang digunakan untuk mengambil data siswa, biasanya diambil dari Room Database atau sumber data lain.
    
    homeUiState adalah StateFlow yang digunakan untuk menyimpan status UI untuk daftar siswa. StateFlow memungkinkan tampilan untuk mengamati perubahan data secara reaktif.

---
+++ app/src/main/java/com/example/roomp9/viewmodel/HomeViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.roomp9.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomp9.repositori.RepositoriSiswa
+import com.example.roomp9.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlin.collections.toList
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream().filterNotNull()
+        .map {HomeUiState(listSiswa = it.toList())}
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
