17a8fbbc1f3e51c353a57301dc656eeb8d569e11
RMadya <kereyoben75@gmail.com>
2025-11-27 16:12:59 +0700

    string dan siswa dan siswa dao serta DatabaseSiswa

--- app/src/main/java/com/example/roomp9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomp9/room/DatabaseSiswa.kt
@@ -5,5 +5,22 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class  DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
 
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context) : DatabaseSiswa {
+            return  (Instance?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
+}
 
