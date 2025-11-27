02f33d08040671306b32391d0ba13fcce99602c2
RMadya <kereyoben75@gmail.com>
2025-11-28 01:41:54 +0700

    Ini digunakan untuk mendefinisikan informasi terkait rute dan judul untuk tampilan atau halaman Home dalam aplikasi.

---
+++ app/src/main/java/com/example/roomp9/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.roomp9.view.route
+
+import com.example.roomp9.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
