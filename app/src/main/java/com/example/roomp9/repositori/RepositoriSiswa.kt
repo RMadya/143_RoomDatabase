2033ba797754ac9a0569b4c10230389a11372585
RMadya <kereyoben75@gmail.com>
2025-11-28 01:35:40 +0700

    RepositoriSiswa adalah interface yang mendefinisikan kontrak untuk operasi yang dapat dilakukan pada data Siswa

--- app/src/main/java/com/example/roomp9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomp9/repositori/RepositoriSiswa.kt
@@ -2,16 +2,13 @@ package com.example.roomp9.repositori
 
 import com.example.roomp9.room.Siswa
 import com.example.roomp9.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
-    suspend fun insertSiswa(siswa: Siswa)
+    suspend fun InsertSiswa(siswa: Siswa)
 }
 
-class OffLineRepositoriSiswa(
-    private val siswaDao: SiswaDao
-): RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
-}
+
+
