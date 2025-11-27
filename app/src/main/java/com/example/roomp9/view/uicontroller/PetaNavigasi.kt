4f8409412ce5a03325a486d9178f46811cf755c1
RMadya <kereyoben75@gmail.com>
2025-11-28 01:43:53 +0700

    SiswaApp adalah fungsi Composable utama yang akan menampilkan NavHost yang berfungsi sebagai pengatur navigasi aplikasi.

---
+++ app/src/main/java/com/example/roomp9/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,19 @@
+package com.example.roomp9.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomp9.view.EntrySiswaScreen
+import com.example.roomp9.view.HomeScreen
+import com.example.roomp9.view.route.DestinasiEntry
+import com.example.roomp9.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
